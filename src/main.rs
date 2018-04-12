3ddd9b28304c59d632bf5273e0daaec5132c26ab
Teddy Wing <git@teddywing.com>
2018-04-12 22:31:36 +0200

    Parse command line arguments and add help output
    Include an option to pass a Meetup.com API token (not saved yet).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate getopts;
+
+use getopts::Options;
+
+use std::env;
+use std::process::exit;
+
+fn print_usage(opts: Options) {
+    let brief = "usage: meetup-find-events-rss";
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optopt(
+        "",
+        "meetup-api-token",
+        "Meetup.com API token (required)",
+        "TOKEN"
+    );
+    opts.optflag("h", "help", "print this help menu");
+
+    let opt_matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            eprint!("meetup-find-events-rss: error: {}", f.to_string());
+            exit(1);
+        },
+    };
+
+    if opt_matches.opt_present("h") {
+        print_usage(opts);
+        return;
+    }
 }
