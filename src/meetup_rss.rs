f179d813e1656c2bca07bbb8c86a3a53278044c8
Teddy Wing <git@teddywing.com>
2018-04-13 18:32:53 +0200

    meetup(generate): Improve test and take a reference argument
    * Take a `Vec` reference instead of an object. This is the result of
      messing around with calling the function in the test.
    * Update the test to actually make assertions on the result of the
      function.

--- src/meetup_rss.rs
+++ src/meetup_rss.rs
@@ -2,12 +2,12 @@ use rss::{Channel, ChannelBuilder, Item};
 
 use meetup::event::Event;
 
-pub fn generate(events: Vec<Event>) -> Result<Channel, String> {
+pub fn generate(events: &Vec<Event>) -> Result<Channel, String> {
     let items: Vec<Item> = events.into_iter().map(|event| {
         let mut item = Item::default();
-        item.set_title(event.name);
-        item.set_link(event.link);
-        item.set_description(event.description);
+        item.set_title(event.name.clone());
+        item.set_link(event.link.clone());
+        item.set_description(event.description.clone());
 
         item
     }).collect();
@@ -27,18 +27,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_generate_builds_a_channel() {
-        let x = generate(
-            vec![
-                Event {
-                    name: "Summer Sun Celebration".to_owned(),
-                    description: "Description".to_owned(),
-                    link: "http://example.com".to_owned(),
-                    local_date: "2018-04-13".to_owned(),
-                    local_time: "18:30".to_owned(),
-                }
-            ]
-        );
-        println!("{:?}", x);
+    fn test_generate_builds_a_channel_of_events() {
+        let events = vec![
+            Event {
+                name: "Summer Sun Celebration".to_owned(),
+                description: "Description".to_owned(),
+                link: "http://example.com".to_owned(),
+                local_date: "2018-04-13".to_owned(),
+                local_time: "18:30".to_owned(),
+            }
+        ];
+        let event = &events[0];
+
+        let channel = generate(&events).unwrap();
+
+        let item = channel.items().first().unwrap();
+
+        assert_eq!(event.name, item.title().unwrap());
+        assert_eq!(event.link, item.link().unwrap());
+        assert_eq!(event.description, item.description().unwrap());
     }
 }
